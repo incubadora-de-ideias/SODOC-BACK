ebb2acafe72d535088a6141c7a95061cc8b08e52
josecapewa <capewajose@gmail.com>
2025-01-08 21:32:23 +0100

    uploading files

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { authService } from "../../modules/services/auth";
 import { userService } from "../../modules/services/user";
+import { users } from "./user.routes";
 
 export default async function routes(app: FastifyInstance) {
     app.post("/auth/login", authService.login);
-    app.post("/user", userService.create);
     app.register(async (privateRoute) => {
         privateRoute.delete("/auth/logout", authService.logout);
         privateRoute.addHook("preHandler", authService.authenticate);
+        await users(privateRoute);
     })
 }
