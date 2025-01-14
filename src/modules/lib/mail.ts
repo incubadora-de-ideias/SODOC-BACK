e5395fc99322f4ba0a4de20cd1856e727d8a2d9e
pascoalnzola <pascoalnzolatondo@gmail.com>
2025-01-14 14:47:40 +0100

    filter

--- src/modules/lib/mail.ts
+++ src/modules/lib/mail.ts
@@ -1,6 +1,3 @@
-import { DestinoNotificacao } from "@prisma/client";
-import { notificationModel } from "../models/notification";
-import { userModel } from "../models/user";
 import { mailService } from "../services/mail";
 import { emailOptions } from "../templates/email";
 import { IMailService, TemplateVariables } from "../types/mail";
@@ -14,23 +11,6 @@ class ValidateUserEmailUseCase {
     variables: Record<TemplateVariables[T], string>
   ) {
     await this.mailService.send(type, email, variables);
-
-    const usuario = await userModel.getIdByEmial(email);
-
-    if (!usuario) {
-      throw new Error(`User with email ${email} not found`);
-    }
-   const {descricao, titulo} = await notificationModel.getTitleAndDescrition(type); 
-    const notificacao = {
-        id_usuario: usuario.id,
-        titulo: titulo,
-        descricao: descricao, 
-        tipo: type,
-        destino: 'EXTERNO' as DestinoNotificacao, 
-    };
-    const sendNotification = await notificationModel.create(notificacao);
-    
-    return notificacao;
   }
 }
 
